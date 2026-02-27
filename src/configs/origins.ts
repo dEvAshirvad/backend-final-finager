d9dbe1b731347d6979292a933f14eec5b7f65189
Ashirvad Satapathy <74089931+dEvAshirvad@users.noreply.github.com>
2026-02-27 13:00:13 +0530

    feat: origins

--- src/configs/origins.ts
+++ src/configs/origins.ts
@@ -1,15 +1,16 @@
 const allowedOrigins = [
-	"http://localhost:3000",
-	"http://localhost:3001",
-	"http://localhost:3002",
-	"http://localhost:3003",
-	"http://localhost:3004",
-	"http://localhost:3005",
-	"http://localhost:3006",
-	"http://localhost:3007",
-	"http://localhost:3008",
-	"http://localhost:3009",
-	"http://localhost:3010",
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'http://localhost:3002',
+  'http://localhost:3111',
+  'http://localhost:3112',
+  'http://72.60.222.228:3000',
+  'http://72.60.222.228:3001',
+  'http://72.60.222.228:3002',
+  'http://72.60.222.228:3111',
+  'http://72.60.222.228:3112',
+  'https://www.finagerindia.com',
+  'https://api.finagerindia.com',
 ];
 
 export default allowedOrigins;
